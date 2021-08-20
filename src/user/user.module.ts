686de922a66e681d0cf294f7553e83df8f01e46a
Piyush Mehta <me@piyushmehta.com>
2021-08-20 22:50:36 +0530

    [WIP] users service

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,5 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 
-@Module({ providers: [UserService], exports: [UserService] })
+@Module({
+  imports: [],
+  providers: [UserService],
+  exports: [UserService],
+})
 export class UserModule {}
